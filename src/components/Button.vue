6fc5f749d23758c0f3a0a597b054aae67b76b148
KingLyrics <ekom338.com@gmail.com>
2022-10-26 18:07:04 +0100

    Added button and header components

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button :style="{background:color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'button',
+    props: {
+        text:String,
+        color:String,
+    }
+}
+</script>
+
+
+<style>
+    
+</style>
